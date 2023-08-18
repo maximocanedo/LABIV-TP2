cd21cf00f0a4a3e9c1895fdd5a8deca68dd2e270
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-08-17 23:12:23 -0300

    Encapsulación. Closes #3

--- TP_2/src/ejercicio1/Profesor.java
+++ TP_2/src/ejercicio1/Profesor.java
@@ -1,6 +1,20 @@
 package ejercicio1;
 
 public class Profesor extends Empleado{
-	String cargo;
-	int antiguedadDocente;
+	private String cargo;
+	private int antiguedadDocente;
+	
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
 }
