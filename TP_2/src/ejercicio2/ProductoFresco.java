702cf34b935ff7710e8c033061d26ea1929fdb5b
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-08-24 22:57:43 -0300

    Más constructores y fechas

--- TP_2/src/ejercicio2/ProductoFresco.java
+++ TP_2/src/ejercicio2/ProductoFresco.java
@@ -7,21 +7,61 @@ public class ProductoFresco extends Producto {
 	private String paisOrigen;
 	
 	
-	@Override
-	public String toString() {
-		return super.toString()+ " ProductoFresco | fecha Envasado: " + 
-	fechaEnvasado + ", pais Origen: " + paisOrigen;
+	public ProductoFresco(String po, Date fe, int lote, Date fv) {
+		super(lote, fv);
+		setPaisOrigen(po);
+		setFechaEnvasado(fe);
 	}
-	public ProductoFresco(){
-		this(null,"argentina");
+	public ProductoFresco(String po, Date fe, int lote, Calendar fv) {
+		super(lote, fv);
+		setPaisOrigen(po);
+		setFechaEnvasado(fe);
 	}
-	public ProductoFresco(Date fecha,String pais){
-		this.fechaEnvasado=fecha;
-		this.paisOrigen=pais;
+	public ProductoFresco(String po, Calendar fe, int lote, Date fv) {
+		super(lote, fv);
+		setPaisOrigen(po);
+		setFechaEnvasado(fe.getTime());
 	}
-	
-	
-	
+	public ProductoFresco(String po, Calendar fe, int lote, Calendar fv) {
+		super(lote, fv);
+		setPaisOrigen(po);
+		setFechaEnvasado(fe.getTime());
+	}
+	public ProductoFresco(String po, Date fe, int lote) {
+		super(lote);
+		setPaisOrigen(po);
+		setFechaEnvasado(fe);
+	}
+	public ProductoFresco(String po, Calendar fe, int lote) {
+		super(lote);
+		setPaisOrigen(po);
+		setFechaEnvasado(fe.getTime());
+	}
+	public ProductoFresco(String po, Date fe) {
+		super();
+		setPaisOrigen(po);
+		setFechaEnvasado(fe);
+	}
+	public ProductoFresco(String po, Calendar fe) {
+		super();
+		setPaisOrigen(po);
+		setFechaEnvasado(fe.getTime());
+	}
+	public ProductoFresco(String po) {
+		super();
+		setPaisOrigen(po);
+		Calendar fechaActual = Calendar.getInstance();
+        fechaActual.add(Calendar.YEAR, -1);
+		setFechaEnvasado(fechaActual.getTime());
+	}
+	public ProductoFresco() {
+		super();
+		setPaisOrigen("AR");
+		Calendar fechaActual = Calendar.getInstance();
+        fechaActual.add(Calendar.YEAR, -1);
+		setFechaEnvasado(fechaActual.getTime());
+	}
+		
 	//GETTERS SETTERS
 	public Date getFechaEnvasado() {
 		return fechaEnvasado;
@@ -34,6 +74,11 @@ public class ProductoFresco extends Producto {
 	}
 	public void setPaisOrigen(String paisOrigen) {
 		this.paisOrigen = paisOrigen;
+	}
+	@Override
+	public String toString() {
+		return super.toString()+ " ProductoFresco | fecha Envasado: " + 
+	fechaEnvasado + ", pais Origen: " + paisOrigen;
 	}
 	
 	
